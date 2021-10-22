cd86122f231910d305415f5640fe5445d3635a74
imykhailychenko <igor.c.m@ukr.net>
2021-10-22 16:28:05 +0300

    init

---
+++ src/root/root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Root = ({ test }) => {
+  console.log(test);
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eligendi error est fugiat iure maxime molestiae nobis
+      odit, pariatur reiciendis rem sit, unde velit? Earum labore magnam quas quos voluptas!
+    </div>
+  );
+};
+
+export default Root;
