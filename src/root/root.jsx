8a8b605881ba6ebd7801845de00b33c753396d84
imykhailychenko <igor.c.m@ukr.net>
2021-10-22 16:34:32 +0300

    init

--- src/root/root.jsx
+++ src/root/root.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Root = () => {
-  return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eligendi error est fugiat iure maxime molestiae nobis
-      odit, pariatur reiciendis rem sit, unde velit? Earum labore magnam quas quos voluptas!
-    </div>
-  );
-};
+const Root = () => (
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eligendi error est fugiat iure maxime molestiae nobis
+    odit, pariatur reiciendis rem sit, unde velit? Earum labore magnam quas quos voluptas!
+  </div>
+);
 
 export default Root;
