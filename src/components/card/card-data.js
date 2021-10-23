c110ee29eddd9958405ae0ef228add33fe84b0d9
Oleksandr <a.a.vdovkin@gmail.com>
2021-10-23 15:54:20 +0300

    add rating component

---
+++ src/components/card/card-data.js
@@ -0,0 +1,21 @@
+const CARD_DATA = [
+          {
+            id: 1,
+            name: 'Cafe1',
+            imageUrl: 'https://cdn.pixabay.com/photo/2016/02/19/11/40/coffee-shop-1209863_960_720.jpg',
+          },
+          {
+            id: 2,
+            name: 'Cafe2',
+            imageUrl: 'https://cdn.pixabay.com/photo/2016/02/19/11/40/coffee-shop-1209863_960_720.jpg',
+          },
+          {
+            id: 3,
+            name: 'Cafe3',
+            imageUrl: 'https://cdn.pixabay.com/photo/2016/02/19/11/40/coffee-shop-1209863_960_720.jpg',
+          }
+          
+        ]
+      
+
+export default CARD_DATA;
