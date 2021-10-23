4b29c817dadad49593dc386facbc4ea8814e26a8
Oleksandr <a.a.vdovkin@gmail.com>
2021-10-23 16:01:22 +0300

    removed unnecessary fiels

--- src/components/card/rating.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import Rating from '@mui/material/Rating';
-
-const RatingComponent = ({ value }) => (
-    <Rating name="read-only" value={value} readOnly />
-);
-
-export default RatingComponent;
