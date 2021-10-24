319345765b286038b96d8d80f7da679d013c1cd1
imykhailychenko <igor.c.m@ukr.net>
2021-10-24 15:01:32 +0300

    add redux

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import auth from './entities/auth/auth.reducer';
 import profile from './entities/profile/profile.reducer';
+import users from './entities/users/users.reducer';
 import { IState } from './interfaces';
 
 const rootReducer = combineReducers<IState>({
     auth,
     profile,
+    users,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
