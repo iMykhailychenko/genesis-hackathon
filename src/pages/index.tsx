8bff3361140f707394599cc270e0adc1acca5ed8
Julia <mjulia1794@gmail.com>
2021-10-24 11:05:30 +0300

    filters & sorting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import CardList from '../components/common/card-list/card-list';
-import Filters from '../components/filters/Filters';
 import Banner from '../components/pages/home/banner';
 
 const Home = (): JSX.Element => {
