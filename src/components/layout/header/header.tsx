655023678eff904fbf4fca520e936fef681ff87f
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2021-10-24 15:07:23 +0300

    Replace router.push

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -10,7 +10,6 @@ import IconButton from '@mui/material/IconButton';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Toolbar from '@mui/material/Toolbar';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import { LANGUAGE_ENUM } from '../../../interfaces';
@@ -27,7 +26,14 @@ const Header = (): JSX.Element => {
     return (
         <AppBar position="static" sx={{ bgcolor: 'primary.main' }}>
             <Toolbar>
-                <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                <IconButton
+                    onClick={() => history.push('/')}
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
                     <img src="/images/icons_cafeteria.svg" alt="logo" />
                 </IconButton>
 
@@ -61,12 +67,8 @@ const Header = (): JSX.Element => {
                 </Button> */}
 
                 <ButtonGroup variant="contained">
-                    <Button>
-                        <Link href="/login">Login</Link>
-                    </Button>
-                    <Button>
-                        <Link href="/registration">Join</Link>
-                    </Button>
+                    <Button onClick={() => history.push('/login')}>Login</Button>
+                    <Button onClick={() => history.push('/registration')}>Join</Button>
                 </ButtonGroup>
             </Toolbar>
         </AppBar>
