c0b14fda558de858164d1b7a8665f9903c95106a
imykhailychenko <igor.c.m@ukr.net>
2021-10-24 15:28:33 +0300

    merge

--- src/state/initial-state.ts
+++ src/state/initial-state.ts
@@ -1,10 +1,12 @@
 import authInitialState from './entities/auth/auth.initial-state';
 import { profileInitialState } from './entities/profile/profile.initial-state';
+import { usersInitialState } from './entities/users/users.initial-state';
 import { IState } from './interfaces';
 
 const rootInitialState: IState = {
     auth: authInitialState,
     profile: profileInitialState,
+    users: usersInitialState,
 };
 
 export default rootInitialState;
--- src/state/interfaces.ts
+++ src/state/interfaces.ts
@@ -2,12 +2,14 @@ import { Dispatch } from 'redux';
 
 import { IAuthState } from './entities/auth/auth.interface';
 import { IProfileInfoState } from './entities/profile/profile.interface';
+import { IUserState } from './entities/users/users.interface';
 
 export type ThunkStatuses = 'idle' | 'loading' | 'success' | 'error';
 
 export interface IState {
     auth: IAuthState;
     profile: IProfileInfoState;
+    users: IUserState,
 }
 
 export type AsyncThunkConfig = {
