666b2f6e5fdd0cb994d9af51c0ce322739e640c8
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2021-10-24 11:56:23 +0300

    Resolove main

--- src/context/root-provider.tsx
+++ src/context/root-provider.tsx
@@ -1,22 +1,21 @@
 import React, { ReactElement } from 'react';
 
 import { IAuth } from '../interfaces/auth';
-
-import { AuthProvider } from './auth/auth';
-
-
 import { Store } from '../store/store';
 
+import { AuthProvider } from './auth/auth';
 import StoreProvider from './store/store';
 
 interface IProps {
     store: Store;
-        auth: IAuth;
-    children:JSX.Element[] | JSX.Element;
+    auth: IAuth;
+    children: JSX.Element[] | JSX.Element;
 }
 
 const RootProvider = ({ store, auth, children }: IProps): ReactElement => (
-   <StoreProvider initialState={store}> <AuthProvider myAuth={auth}>{children}</AuthProvider></StoreProvider>;
+    <StoreProvider initialState={store}>
+        <AuthProvider myAuth={auth}>{children}</AuthProvider>
+    </StoreProvider>
 );
 
 export default RootProvider;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,12 @@ import '../styles/root.scss';
 import React from 'react';
 
 import type { AppProps } from 'next/app';
+import App from 'next/app';
+import { AppContextType } from 'next/dist/next-server/lib/utils';
+import { Router } from 'next/router';
 
-import { getUser } from '../context/auth/auth';
-
-        
 import RootLayout from '../components/layout/root-layout/root-layout';
-
-
+import { getUser } from '../context/auth/auth';
 import RootProvider from '../context/root-provider';
 import interceptor from '../helpers/interceptor.helper';
 
@@ -17,19 +16,17 @@ interface IProps {
     auth: any;
 }
 
-
 const MyApp = ({ Component, pageProps, auth }: AppProps & IProps): JSX.Element => {
     interceptor();
 
     return (
-        <RootProvider  auth={auth} store={pageProps}>
+        <RootProvider auth={auth} store={pageProps}>
             <RootLayout>
                 <Component {...pageProps} />
             </RootLayout>
         </RootProvider>
     );
 };
-
 
 MyApp.getInitialProps = (appContext: AppContextType<Router>): Promise<IProps> => {
     return Promise.all([App.getInitialProps(appContext), getUser(appContext.ctx)])
@@ -41,6 +38,5 @@ MyApp.getInitialProps = (appContext: AppContextType<Router>): Promise<IProps> =>
             process.exit(1);
         });
 };
-
 
 export default MyApp;
